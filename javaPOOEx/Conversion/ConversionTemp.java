c92494716f1847d5d62fd722864902250979673c
NeruChan2003 <167128377+NeruChan2003@users.noreply.github.com>
2024-05-08 18:34:40 -0600

    Update ConversionTemp.java

--- javaPOOEx/Conversion/ConversionTemp.java
+++ javaPOOEx/Conversion/ConversionTemp.java
@@ -1,7 +1,11 @@
+//Se define la clase
 public class ConversionTemp {
+    //Un atributo privado que almacena la temperatura en grados Celsius.
     private double celsius;
+    //Un atributo privado que almacena la temperatura en grados Fahrenheit.
     private double fahrenheit;
 
+    //Un método público que devuelve la temperatura en grados Celsius.
     public ConversionTemp(double celsius) {
         this.celsius = celsius;
     }
@@ -10,22 +14,27 @@ public class ConversionTemp {
         return celsius;
     }
 
+    // Un método público que establece la temperatura en grados Celsius.
     public void setCelsius(double celsius) {
         this.celsius = celsius;
     }
 
+    // Un método público que devuelve la temperatura en grados Fahrenheit.
     public double getFahrenheit() {
         return fahrenheit;
     }
 
+    //Un método público que establece la temperatura en grados Fahrenheit.
     public void setFahrenheit(double fahrenheit) {
         this.fahrenheit = fahrenheit;
     }
 
+    //Un método público que convierte la temperatura de grados Celsius a grados Fahrenheit utilizando la fórmula de conversión.
     public double convertToFahrenheit() {
         return (9 * celsius + 160) / 5;
     }
 
+    //método público que calcula la temperatura en grados Fahrenheit llamando a convertToFahrenheit()y luego muestra ambos valores de temperatura en la consola.
     public void mostrarFahrenheit() {
         fahrenheit = convertToFahrenheit();
         System.out.println("Grados en Celsius: " + celsius);
@@ -33,7 +42,15 @@ public class ConversionTemp {
     }
 
     @Override
+    //devuelve una representación en cadena de caracteres de los detalles del objeto. Esto incluye el número de días y el total de segundos calculados.
     public String toString() {
         return "ConversionTemp [celsius=" + celsius + ", fahrenheit=" + fahrenheit + "]";
     }
 }
+
+
+
+
+
+
+
