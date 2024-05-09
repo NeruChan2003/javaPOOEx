beb688edbb4894dc6d62f3aec5dc45bff0ef7ed1
NeruChan2003 <167128377+NeruChan2003@users.noreply.github.com>
2024-05-08 18:45:57 -0600

    Update AreaCirculo.java

--- javaPOOEx/Area del circulo/AreaCirculo.java
+++ javaPOOEx/Area del circulo/AreaCirculo.java
@@ -1,14 +1,17 @@
+//Se define la clase
 public class AreaCirculo {
+     //Un atributo privado que almacena la radio del círcul
     private double radio;
-
+    //Constructor
     public AreaCirculo(double radio) {
         this.radio = radio;
     }
-
+    //Un método público que devuelve la radio del círculo.
     public double getRadio() {
         return radio;
     }
 
+    //Un método público que establece la radio del círculo.
     public void setRadio(double radio) {
         this.radio = radio;
     }
@@ -17,6 +20,7 @@ public class AreaCirculo {
         return calcularAreaRecursiva(radio);
     }
 
+    //método privado que calcula el área del círculo utilizando la fórmula π * r^2. Esteradiohasta 1. Cuando radioes menor o igual a 0, devuelve 0.
     private double calcularAreaRecursiva(double radio) {
         if (radio <= 0) {
             return 0;
@@ -24,9 +28,14 @@ public class AreaCirculo {
         return Math.PI * Math.pow(radio, 2) + calcularAreaRecursiva(radio - 1);
     }
 
+    //Un método sobrescrito que devuelve una representación en cadena de caracteres del objeto AreaCirculo,
     @Override
     public String toString() {
         double area = calcularArea();
         return "AreaCirculo{" + "radio=" + radio + ", area=" + area + '}';
     }
 }
+
+
+
+
